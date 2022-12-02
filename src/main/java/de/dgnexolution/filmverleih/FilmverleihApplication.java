52bb24f50501d1a32dbb6ed62d03742a8a2dbad2
agoenkur <agoenkur@dgverlag.de>
2022-12-02 14:25:50 +0100

    create SpringBoot project with SpringInitializr

---
+++ src/main/java/de/dgnexolution/filmverleih/FilmverleihApplication.java
@@ -0,0 +1,13 @@
+package de.dgnexolution.filmverleih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmverleihApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmverleihApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dgnexolution/filmverleih/FilmverleihApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dgnexolution.filmverleih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmverleihApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
